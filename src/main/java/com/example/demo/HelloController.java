03e00defa5963d2fb01d42929837e0c1f3763064
zenos879 <lenovo879@gmail.com>
2019-12-10 14:44:04 +0800

    增加测试页面

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping("/test")
 	public String hello() {
 		return "Hello world! Welcome to start SpringBoot!";
 	}
